deed5782dddeac07ae8abcfc59a9d81ed94ca8af
Morgan <lilmorgan256@gmail.com>
2026-06-14 16:59:14 +0300

    ""

--- src/app.js
+++ src/app.js
@@ -30,7 +30,22 @@ const allowedOrigins = [
 ];
 
 // CORS middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.log("Blocked origin:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // React dev server
 app.use(helmet());
