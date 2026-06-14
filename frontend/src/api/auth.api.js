6734da26a5129ffb37cb866171c37f90742b94bb
Morgan <lilmorgan256@gmail.com>
2026-06-14 15:38:11 +0300

    fixing deploymnet bugs

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -1,6 +1,7 @@
 import api from "./axios.js";
 
-export const register = (data) => api.post("/auth/register", data);
-export const login = (data) => api.post("/auth/login", data);
-export const getMe = () => api.get("/auth/me");
-export const updatePassword = (data) => api.put("/auth/update-password", data);
+export const register = (data) => api.post("/api/auth/register", data);
+export const login = (data) => api.post("/api/auth/login", data);
+export const getMe = () => api.get("/api/auth/me");
+export const updatePassword = (data) =>
+  api.put("/api/auth/update-password", data);
